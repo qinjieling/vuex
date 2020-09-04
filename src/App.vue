3cf1b6689989593a5d605e3094d4c884f5d15f75
chenkeda <18868195531@163.com>
2020-09-04 19:09:09 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,89 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-input placeholder="请输入任务" v-model="inputValue" class="my_ipt" />
+    <a-button type="primary" @click="addItems()">添加事项</a-button>
+
+    <a-list bordered :dataSource="list" class="dt_list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <!-- 复选框 -->
+        <a-checkbox :check="item.done" @click="checkItem(item)">{{item.info}}</a-checkbox>
+        <!-- 删除链接 -->
+        <a slot="actions">删除</a>
+      </a-list-item>
+
+      <!-- footer区域 -->
+      <div slot="footer" class="footer">
+        <!-- 未完成的任务个数 -->
+        <span>{{unDoneLeft}}条剩余</span>
+        <!-- 操作按钮 -->
+        <a-button-group>
+          <a-button type="primary">全部</a-button>
+          <a-button>未完成</a-button>
+          <a-button>已完成</a-button>
+        </a-button-group>
+        <!-- 把已经完成的任务清空 -->
+        <a>清除已完成</a>
+      </div>
+    </a-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import{mapState, mapGetters,mapActions} from 'vuex'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data() {
+    return {
+      // list: []
+      inputValue:''
+    }
+  },
+  created(){
+    this.$store.dispatch('getList')
+    
+  },
+  mounted(){
+    // ...mapActions(['getList'])
+  },
+  computed:{
+    ...mapState(['list']),
+    ...mapGetters(['unDoneLeft'])
+  },
+  methods:{
+    addItems(){
+      console.log(this.inputValue)
+      if(this.inputValue.trim().length >0){
+        this.$store.commit('addlist',this.inputValue)
+        this.inputValue = ''
+      }else{
+        alert('输入不能为空！')
+      }
+    },
+    checkItem( item ){
+      item.done = !item.done
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 10px;
+}
+
+.my_ipt {
+  width: 500px;
+  margin-right: 10px;
+}
+
+.dt_list {
+  width: 500px;
+  margin-top: 10px;
+}
+
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,38 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    list:[],
+    newid:5
   },
   mutations: {
+    initList(state,list){
+      state.list = list
+    },
+    addlist(state,value){
+      let json = {
+          "id": state.newid++,
+          "info": value,
+          "done": false
+      }
+      state.list.push(json)
+    }
   },
   actions: {
+    getList(context){
+      axios.get('/list.json').then(({data})=>{
+        console.log(data)
+        context.commit('initList',data)
+      })
+    }
+  },
+  getters:{
+    unDoneLeft:state => {
+      return state.list.filter(v=>{ return v.done }).length
+    }
   },
   modules: {
   }
